5a2231ac6feba6866ff6549b170200aec99d904f
Humayun Kabir <humayunkabir6267@gmail.com>
2026-08-19 17:33:40 +0600

    feat: trapping rain water optimize solution added

--- solutions/leetcode/trapping_rain_water.cpp
+++ solutions/leetcode/trapping_rain_water.cpp
@@ -22,10 +22,37 @@ int trapNaive(vector<int> &height)
     }
     return result;
 }
+
+// optimize solution
+int trap(vector<int> &height)
+{
+    int result = 0;
+    int l = 0;
+    int r = height.size() - 1;
+    int lmax = 0;
+    int rmax = 0;
+    while (l < r)
+    {
+        lmax = max(lmax, height[l]);
+        rmax = max(rmax, height[r]);
+        if (lmax < rmax)
+        {
+            result += lmax - height[l];
+            l++;
+        }
+        else
+        {
+            result += rmax - height[r];
+            r--;
+        }
+    }
+    return result;
+}
 int main()
 {
     vector<int> height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
     cout << trapNaive(height) << endl;
+    cout << trap(height) << endl;
 
     return 0;
 }
