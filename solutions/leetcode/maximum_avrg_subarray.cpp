bf49e4e8cf4c9504ddc6d2b90f5fe9d31d05bd1b
Humayun Kabir <humayunkabir6267@gmail.com>
2026-08-21 21:01:13 +0600

    feat: different way to solution of max average window

--- solutions/leetcode/maximum_avrg_subarray.cpp
+++ solutions/leetcode/maximum_avrg_subarray.cpp
@@ -20,11 +20,28 @@ double findMaxAverage(vector<int>& nums, int k) {
     return (double)maxSum / k;
 }
 
+double findMaxAverageSlidingWindow(vector<int>& nums, int k) {
+    long long sum = 0;
+
+    for (int i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+
+    long long maxSum = sum;
+
+    for (int i = k; i < nums.size(); i++) {
+        sum += nums[i] - nums[i - k];
+        maxSum = max(maxSum, sum);
+    }
+
+    return (double)maxSum / k;
+}
+
 int main() {
     vector<int> nums = {1, 2, 3, 4, 5};
     int k = 3;
 
-    double result = findMaxAverage(nums, k);
+    double result = findMaxAverageSlidingWindow(nums, k);
     cout << "Maximum average: " << result << endl;
 
     return 0;
