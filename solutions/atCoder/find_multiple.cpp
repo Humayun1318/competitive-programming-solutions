a8dd81b7bec13b97dc1ec6f5c619fa5d0cefd3e2
Humayun Kabir <humayunkabir6267@gmail.com>
2026-08-23 14:12:06 +0600

    feat: find multiple of third number between first and second number solution optimize and bruteforce

--- solutions/atCoder/find_multiple.cpp
+++ solutions/atCoder/find_multiple.cpp
@@ -1,13 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int findMultiple(int a, int c){
+    return ((a + c - 1) / c) * c;
+}
+
+// brute force solution
+int findMultipleBruteforce(int a, int b, int c){
+    for(int i=a; i<=b; i++){
+        if(i%2 == 0){
+            return i;
+        }
+    }
+
+    return -1;
+}
+
 int main() {
 
     int a,b,c; cin >> a >> b >> c;
-    if(2*c > a || (a < 2*c && 2*c <= b)){
-        cout << 2*c << "\n";
+    int ans = findMultiple(a,c);
+    // int ans = findMultipleBruteforce(a, b, c);
+    if(a < ans && ans <= b){
+        cout << ans << "\n";
     }else{
-        cout << "-1" << "\n";
+        cout << -1 << "\n";
     }
 
     return 0;
