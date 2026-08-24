86187a41919422e49fd6d57c16d265ff66bd50df
Humayun Kabir <humayunkabir6267@gmail.com>
2026-08-24 13:37:57 +0600

    feat: optimize solution for four digits

--- solutions/atCoder/four_digits.cpp
+++ solutions/atCoder/four_digits.cpp
@@ -4,14 +4,15 @@ using namespace std;
 int main() {
 
     int n; cin >> n;
-    if(n < 10)
-        cout << 0<< 0<< 0 << n;
-    else if (n < 100)
-        cout << 0<<0 << n;
-    else if(n < 1000)
-        cout << 0 << n;
-    else
-        cout << n;
+    // if(n < 10)
+    //     cout << 0<< 0<< 0 << n;
+    // else if (n < 100)
+    //     cout << 0<<0 << n;
+    // else if(n < 1000)
+    //     cout << 0 << n;
+    // else
+    //     cout << n;
+    cout << setfill('0') << setw(4) << n;
 
     return 0;
 }
