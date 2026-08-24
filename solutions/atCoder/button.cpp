80fd8ca0f04053a02bedfb0893beb1f43f8374b1
Humayun Kabir <humayunkabir6267@gmail.com>
2026-08-24 12:59:51 +0600

    feat: explaination in comment of button problem solutions

--- solutions/atCoder/button.cpp
+++ solutions/atCoder/button.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 
-
+//there possibilities to have 3 scenarios 
+// first, a 2 times press that time (a+a-1 = 2a-1)
+// second, a 1 times b 1 times press that time (a+b)
+// Third, b 2 times press that time (b+b-1 = 2b-1)
 int main() {
 
     int a, b; cin >> a >> b;
