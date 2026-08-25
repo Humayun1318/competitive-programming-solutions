fa43296777f011361483ada7e011272cb6304738
Humayun Kabir <humayunkabir6267@gmail.com>
2026-08-25 22:42:48 +0600

    feat: not found lexicographically (alphabetically) smallest different solution

--- solutions/atCoder/not_found.cpp
+++ solutions/atCoder/not_found.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+char findMissingLetter(string s) {
+    bool found[26] = {};
+
+    for(char c : s) {
+        found[c - 'a'] = true;
+    }
+
+    for(int i = 0; i < 26; i++) {
+        if(!found[i]) {
+            return char('a' + i);
+        }
+    }
+
+    return 'None';
+}
+
 int main() {
 
     string s1 = "abcdefghijklmnopqrstuvwxyz";
