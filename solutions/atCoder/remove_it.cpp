f3a246d5ea7a272bb6e319eba9b789af2f3a14d5
Humayun Kabir <humayunkabir6267@gmail.com>
2026-08-26 17:16:55 +0600

    feat: space compolexity optimize solution for remove it at coder problem solution

--- solutions/atCoder/remove_it.cpp
+++ solutions/atCoder/remove_it.cpp
@@ -1,22 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
 int main() {
 
-    int n, x; cin >> n  >> x; 
-    vector<int> ar;
-    while(n--){
-        int v; cin >> v;
-        if(v != x){
-            ar.push_back(v);
-        }
+    // int n, x; cin >> n  >> x; 
+    // vector<int> ar;
+    // while(n--){
+    //     int v; cin >> v;
+    //     if(v != x){
+    //         ar.push_back(v);
+    //     }
             
+    // }
+    // if(ar.empty()){
+    //     cout << "";
+    //     return 0;
+    // }
+    // for(auto v: ar)
+    //     cout << v << " " ;
+    int n, x;
+    cin >> n >> x;
+
+    bool first = true;
+
+    while(n--) {
+        int v;
+        cin >> v;
+
+        if(v != x) {
+            if(!first) cout << " ";
+            cout << v;
+            first = false;
+        }
     }
-    if(ar.empty()){
-        cout << "";
-        return 0;
-    }
-    for(auto v: ar)
-        cout << v << " " ;
+
+    cout << '\n';
     return 0;
 }
