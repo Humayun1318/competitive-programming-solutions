1478ce8036bbf5b81f0c3b2010d87f743d48326b
Humayun Kabir <humayunkabir6267@gmail.com>
2026-08-27 20:09:54 +0600

    Feat: codeforces 1883B chemistry solution

---
+++ solutions/codeforces/B_Chemistry.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+
+        int frq[26] = {0};
+        for (auto c : s)
+        {
+            frq[c - 'a']++;
+        }
+
+        int odd = 0;
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (frq[i] % 2 != 0)
+                odd++;
+        }
+
+        if (odd <= k + 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
